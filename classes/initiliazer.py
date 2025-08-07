de00f66332bde88151b888b8939957cdf3ff5df0
IreneMurage <muthonimurage361@gmail.com>
2025-08-07 11:29:13 +0300

    Add implementations for Human class with attributes and methods in JavaScript and Python

---
+++ classes/initiliazer.py
@@ -0,0 +1,41 @@
+class Human:
+
+    def __init__(self):
+        print("Iam human")
+
+    def another_human(self):
+        print("Y.O.L.O")
+
+# Irene = Human()
+Irene = Human()
+Irene.another_human()
+
+
+_______________________________________________________________
+class robot:
+
+    def __init__(self,color,age,name):
+        print(color, age, name)
+
+robot1= robot(color= "red", age = 3, name = "Robo")
+       
+_______________________________________________________________
+#class Name:First letter Cap<> 
+class Student:
+     name="Sam"
+     email="sam@sam.com"
+     phone="25472390423"
+
+#student 1
+student1=Student() #Object datatype: dot notation
+student2=Student()
+
+print("student 1")
+print("name",student1.name)
+print("email",student1.email)
+print("phone",student1.phone)
+
+print("student 2")
+print("name",student2.name)
+print("email",student2.email)
+print("phone",student2.phone)
